b5c90a53ff3a9c72ceab2c8621b12439b5b24fdf
Franklin-Molina <asdfglxl7@gmail.com>
2020-10-04 11:39:11 -0500

    Inicio del proyecto

---
+++ src/components/imagen.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="card" style="heidth: 30rem; heigth=30rem;">
+          <img :src="items.url" class="img-fluid card-img-top" alt="..." />
+          <div class="card-body">
+            <p>{{ items.created_at }}</p>
+          </div>
+        </div>
+</template>
+<script>
+export default {
+    name: "Imagen",
+    props: ["items"],
+}
+</script>
