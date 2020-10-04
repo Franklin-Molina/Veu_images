75ea73eea6d9b0b9160e9cbe5fc0942a59ea33bd
Franklin-Molina <asdfglxl7@gmail.com>
2020-10-04 17:00:10 -0500

    funcional

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,7 +2,10 @@
 <template>
   <div class="container">
     <div>
+
       <h1 class="text-white">Cargar Imagen</h1>
+      <input type="file" @change="onFileSelected">
+      <button @click="onUpload"> Enviar Imagen </button> <br><br>
 
       <li class="btn btn-info">
         <router-link to="/Home">HOME</router-link>
@@ -12,8 +15,29 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: "Index",
+
+  data(){
+    return{
+      selectedFile: null,
+
+    }
+  },
+  methods: {
+    onFileSelected(event){
+      this.selectedFile = event.target.files[0]
+    },
+    onUpload(){
+      const fd = new FormData();
+      fd.append('url',this.selectedFile, this.selectedFile.name)
+      axios.post('https://api.jeisontech.dev/api/images/', fd)
+      .then(res=>{
+        console.log(res)
+      })
+  }
+  }
   
 };
 </script>
